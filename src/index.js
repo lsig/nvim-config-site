df29aaaddff853ecc8d6174fae12e452193b5398
Lsig <logis21@ru.is>
2023-04-25 18:39:48 +0200

    Project setup, hot reload both for js and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+import "./style.css";
 console.log("this works lol");
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,20 @@ module.exports = {
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
+    hot: true,
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
