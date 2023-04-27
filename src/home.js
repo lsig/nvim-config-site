c8c848c2a33c083599c6693bc090cd485e6a53c8
Lsig <logis21@ru.is>
2023-04-27 17:19:22 +0200

    Added a section about setting up a Neovim config

--- src/home.js
+++ src/home.js
@@ -29,9 +29,11 @@ const createBtn = (text, id) => {
 const createSetup = () => {
   const setupDiv = document.createElement("div");
   const install = createInstall();
+  const config = createConfig();
 
   setupDiv.setAttribute("id", "setup");
   setupDiv.appendChild(install);
+  setupDiv.appendChild(config);
   return setupDiv;
 };
 
@@ -50,6 +52,31 @@ const createInstall = () => {
   installDiv.appendChild(installHeader);
   installDiv.appendChild(installParagraph);
   return installDiv;
+};
+
+createConfig = () => {
+  const confDiv = document.createElement("div");
+  const confHeader = document.createElement("h2");
+  const confParagraph = document.createElement("p");
+  const confHeaderText = document.createTextNode(
+    "2. Structuring your Neovim config"
+  );
+  const confText = document.createTextNode(
+    "To setup your Neovim config, go to your .config folder from your home directory. If the directory does not exist, mkdir .config in your homedirectory. Go inside .config and make a folder called nvim. Now make a init.lua file, a folder called lua and a folder called after. Inside the after directory make a folder called plugins, this is where your plugins will be stored and configured. Inside the lua folder make a directory <your-name>, this folder will store your core config files which will be loaded before your plugins. This video by ThePrimeagen (legend) kickstarted my Neovim journey and is extremely helpful, check it out!"
+  );
+  const confLink = document.createElement("a");
+  confLink.href =
+    "https://www.youtube.com/watch?v=w7i4amO_zaE&t=839s&ab_channel=ThePrimeagen";
+  const linkText = document.createTextNode("Primes setup tutorial.");
+
+  confHeader.appendChild(confHeaderText);
+  confParagraph.appendChild(confText);
+  confLink.appendChild(linkText);
+
+  confDiv.appendChild(confHeader);
+  confDiv.appendChild(confParagraph);
+  confDiv.appendChild(confLink);
+  return confDiv;
 };
 
 module.exports = {
